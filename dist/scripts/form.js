2ce88bb2173511de3f22bba11af8fcac6c23bb2a
Mx-Crvr <mcevans.dev@gmail.com>
2023-12-01 09:48:19 +0200

    bug fixes

--- dist/scripts/form.js
+++ dist/scripts/form.js
@@ -1,7 +1,11 @@
 let validLicense = document.getElementById('validLicense');
-const invalidLicenseContainer = document.getElementById('invalidLicenseContainer');
+const invalidLicenseContainer = document.getElementById(
+	'invalidLicenseContainer'
+);
 const invalidLicense = document.getElementById('invalidLicense');
-const licenseSelectContainer = document.getElementById('licenseSelectContainer');
+const licenseSelectContainer = document.getElementById(
+	'licenseSelectContainer'
+);
 // const licenseContainer = document.getElementById('licenseContainer');
 const licenseSelectHTML = document.getElementById('licenseSelect');
 const icaoInput = document.getElementById('icaoInput');
@@ -16,42 +20,41 @@ let selectedValues = [];
 let getURL = window.location.href;
 
 const getSelectedLicense = () => {
-  const licenseSelect = Array.from(licenseSelectHTML.selectedOptions);
-  for (let i =0; i < licenseSelect.length; i++) {
-    selectedValues[i] = licenseSelect[i].value;
-  };
-  
-  if (selectedValues.includes('ICAO')) {
-    icaoInputContainer.classList.remove('hidden');
-  } else if (!selectedValues.includes('ICAO')) {
-    icaoInputContainer.classList.add('hidden');
-  };
+	const licenseSelect = Array.from(licenseSelectHTML.selectedOptions);
+	for (let i = 0; i < licenseSelect.length; i++) {
+		selectedValues[i] = licenseSelect[i].value;
+	}
 
-  if (selectedValues.includes('FAA')) {
-    FAAInputContainer.classList.remove('hidden');
-  } else if (!selectedValues.includes('FAA')) {
-    FAAInputContainer.classList.add('hidden');
-  };
-}
+	if (selectedValues.includes('ICAO')) {
+		icaoInputContainer.classList.remove('hidden');
+	} else if (!selectedValues.includes('ICAO')) {
+		icaoInputContainer.classList.add('hidden');
+	}
+
+	if (selectedValues.includes('FAA')) {
+		FAAInputContainer.classList.remove('hidden');
+	} else if (!selectedValues.includes('FAA')) {
+		FAAInputContainer.classList.add('hidden');
+	}
+};
 
 setInterval(() => {
-  if (validLicense.checked) {
-    invalidLicenseContainer.classList.add('hidden');
-    licenseSelectContainer.classList.remove('hidden');
-    getSelectedLicense();
-    validLicense.value = true;
-  } else {
-    invalidLicenseContainer.classList.remove('hidden');
-    licenseSelectContainer.classList.add('hidden');
-    icaoInputContainer.classList.add('hidden');
-    FAAInputContainer.classList.add('hidden');
-    selectedValues = [];
-  }
+	if (validLicense.checked) {
+		invalidLicenseContainer.classList.add('hidden');
+		licenseSelectContainer.classList.remove('hidden');
+		getSelectedLicense();
+		validLicense.value = true;
+	} else {
+		invalidLicenseContainer.classList.remove('hidden');
+		licenseSelectContainer.classList.add('hidden');
+		icaoInputContainer.classList.add('hidden');
+		FAAInputContainer.classList.add('hidden');
+		selectedValues = [];
+	}
 
-  if (invalidLicense.checked) {
-    validLicense.value = 'false';
-    
-  }
+	if (invalidLicense.checked) {
+		validLicense.value = 'false';
+	}
 }, 100);
 
 // Form validation
@@ -59,99 +62,135 @@ let formHTML = document.querySelectorAll('.form');
 const form = formHTML[0];
 
 form.addEventListener('submit', (e) => {
-  // Validate required fields
-  const requiredFields =  getURL.includes('attendant') ? 
-  ['fname', 'lname', 'email', 'age', 'gender', 'phone', 'age', 'nationality', 'residence', 'vipExperience', 'visa', 'cvUpload', 'photoUpload'] : ['fname', 'lname', 'email', 'age', 'gender', 'phone', 'age', 'nationality', 'residence', 'visa', 'cvUpload', 'photoUpload']
-  ;
-  for (const field of requiredFields) {
-    const inputElement = document.getElementById(field);
-    if (inputElement.value === '') {
-      inputElement.classList.add('border-2', 'border-red', 'p-1');
-      e.preventDefault();
-      return;
-    } else {
-      inputElement.classList.remove('border-2', 'border-red', 'p-1');
-    }
-  };
+	// Validate required fields
+	const requiredFields = getURL.includes('attendant')
+		? [
+				'fname',
+				'lname',
+				'email',
+				'age',
+				'gender',
+				'phone',
+				'age',
+				'nationality',
+				'residence',
+				'vipExperience',
+				'visa',
+				'cvUpload',
+				'photoUpload',
+				'aircraftSelect',
+		  ]
+		: [
+				'fname',
+				'lname',
+				'email',
+				'age',
+				'gender',
+				'phone',
+				'age',
+				'nationality',
+				'residence',
+				'visa',
+				'cvUpload',
+				'photoUpload',
+				'aircraftSelect',
+				'totalTime',
+				'commandTime',
+				'secondTime',
+				'singleEngine',
+				'multiEngine',
+				'jetTime',
+				'turbineTime',
+				'heliHours',
+				'instructorHours',
+		  ];
+	for (const field of requiredFields) {
+		const inputElement = document.getElementById(field);
+		if (inputElement.value === '') {
+			inputElement.classList.add('border-2', 'border-red', 'p-1');
+			e.preventDefault();
+			return;
+		} else {
+			inputElement.classList.remove('border-2', 'border-red', 'p-1');
+		}
+	}
 
-  // Check if the "validLicense" or "invalidLicense" is selected
-  const validLicense = document.getElementById('validLicense');
-  const invalidLicense = document.getElementById('invalidLicense');
-  const licenseContainer = document.getElementById('licenseContainer');
-  const yesNoMessage = document.getElementById('yesNoMessage');
+	// Check if the "validLicense" or "invalidLicense" is selected
+	const validLicense = document.getElementById('validLicense');
+	const invalidLicense = document.getElementById('invalidLicense');
+	const licenseContainer = document.getElementById('licenseContainer');
+	const yesNoMessage = document.getElementById('yesNoMessage');
 
-  if (!validLicense.checked && !invalidLicense.checked) {
-    licenseContainer.classList.add('border-2', 'border-red');
-    yesNoMessage.classList.remove('hidden');
-    e.preventDefault();
-    return;
-  } else {
-    licenseContainer.classList.remove('border-2', 'border-red');
-    yesNoMessage.classList.add('hidden');
-  }
+	if (!validLicense.checked && !invalidLicense.checked) {
+		licenseContainer.classList.add('border-2', 'border-red');
+		yesNoMessage.classList.remove('hidden');
+		e.preventDefault();
+		return;
+	} else {
+		licenseContainer.classList.remove('border-2', 'border-red');
+		yesNoMessage.classList.add('hidden');
+	}
 
-  // Check if the "validLicense" is selected, ensure that the "licenseSelect" field is not empty
-  const licenseSelectHTML = document.getElementById('licenseSelect');
-  const licenseSelectContainer = document.getElementById('licenseSelectContainer');
+	// Check if the "validLicense" is selected, ensure that the "licenseSelect" field is not empty
+	const licenseSelectHTML = document.getElementById('licenseSelect');
+	const licenseSelectContainer = document.getElementById(
+		'licenseSelectContainer'
+	);
 
-  if (validLicense.checked && licenseSelectHTML.value === '') {
-    licenseSelectContainer.classList.add('border-2', 'border-red');
-    e.preventDefault();
-    return;
-  } else {
-    licenseSelectContainer.classList.remove('border-2', 'border-red');
-  }
+	if (validLicense.checked && licenseSelectHTML.value === '') {
+		licenseSelectContainer.classList.add('border-2', 'border-red');
+		e.preventDefault();
+		return;
+	} else {
+		licenseSelectContainer.classList.remove('border-2', 'border-red');
+	}
 
-  // Check if selected licenses include 'ICAO', ensure that the 'icaoInput' field is not empty
-  const selectedValues = Array.from(licenseSelectHTML.selectedOptions).map(option => option.value);
-  const icaoInput = document.getElementById('icaoInput');
-  const icaoInputContainer = document.getElementById('icaoInputContainer');
+	// Check if selected licenses include 'ICAO', ensure that the 'icaoInput' field is not empty
+	const selectedValues = Array.from(licenseSelectHTML.selectedOptions).map(
+		(option) => option.value
+	);
+	const icaoInput = document.getElementById('icaoInput');
+	const icaoInputContainer = document.getElementById('icaoInputContainer');
 
-  if (selectedValues.includes('ICAO') && icaoInput.value === '') {
-    icaoInputContainer.classList.add('border-2', 'border-red');
-    e.preventDefault();
-    return;
-  } else {
-    icaoInputContainer.classList.remove('border-2', 'border-red');
-  }
+	if (selectedValues.includes('ICAO') && icaoInput.value === '') {
+		icaoInputContainer.classList.add('border-2', 'border-red');
+		e.preventDefault();
+		return;
+	} else {
+		icaoInputContainer.classList.remove('border-2', 'border-red');
+	}
 
-  // Check if selected licenses include 'FAA', ensure that the 'FAAInput' field is not empty
-  const FAAInput = document.getElementById('FAAInput');
-  const FAAInputContainer = document.getElementById('FAAInputContainer');
+	// Check if selected licenses include 'FAA', ensure that the 'FAAInput' field is not empty
+	const FAAInput = document.getElementById('FAAInput');
+	const FAAInputContainer = document.getElementById('FAAInputContainer');
 
-  if (selectedValues.includes('FAA') && FAAInput.value === '') {
-    FAAInputContainer.classList.add('border-2', 'border-red');
-    e.preventDefault();
-    return;
-  } else {
-    FAAInputContainer.classList.remove('border-2', 'border-red');
-  }
+	if (selectedValues.includes('FAA') && FAAInput.value === '') {
+		FAAInputContainer.classList.add('border-2', 'border-red');
+		e.preventDefault();
+		return;
+	} else {
+		FAAInputContainer.classList.remove('border-2', 'border-red');
+	}
 
-  // Ensure that at least one of the job types (permanent or freelance) is checked
-  const permanentCheckbox = form.permanent;
-  const freelanceCheckbox = form.freelance;
-  const jobTypeContainer = document.getElementById('jobTypeContainer');
-  const pAndFMessage = document.getElementById('pAndFMessage');
+	// Ensure that at least one of the job types (permanent or freelance) is checked
+	const permanentCheckbox = form.permanent;
+	const freelanceCheckbox = form.freelance;
+	const jobTypeContainer = document.getElementById('jobTypeContainer');
+	const pAndFMessage = document.getElementById('pAndFMessage');
 
-  if (!permanentCheckbox.checked && !freelanceCheckbox.checked) {
-    jobTypeContainer.classList.add('border-2', 'border-red');
-    pAndFMessage.classList.remove('hidden');
-    e.preventDefault();
-    return;
-  } else {
-    jobTypeContainer.classList.remove('border-2', 'border-red');
-    pAndFMessage.classList.add('hidden');
-  }
+	if (!permanentCheckbox.checked && !freelanceCheckbox.checked) {
+		jobTypeContainer.classList.add('border-2', 'border-red');
+		pAndFMessage.classList.remove('hidden');
+		e.preventDefault();
+		return;
+	} else {
+		jobTypeContainer.classList.remove('border-2', 'border-red');
+		pAndFMessage.classList.add('hidden');
+	}
 
-  // Additional checks for other conditions
+	// Additional checks for other conditions
 
-  // ...
+	// ...
 
-  // If all checks pass, the form will be submitted
+	// If all checks pass, the form will be submitted
 });
-
-
-
-
-
-
--- src/server.js
+++ src/server.js
@@ -5,243 +5,255 @@ const Airtable = require('airtable');
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 const upload = multer().fields([
-  { name: 'FAAInput', maxCount: 1 },
-  { name: 'cvUpload', maxCount: 1 },
-  { name: 'videoUpload', maxCount: 1 },
-  { name: 'photoUpload', maxCount: 1 },
+	{ name: 'FAAInput', maxCount: 1 },
+	{ name: 'cvUpload', maxCount: 1 },
+	{ name: 'videoUpload', maxCount: 1 },
+	{ name: 'photoUpload', maxCount: 1 },
 ]);
 const app = express();
 const port = process.env.PORT || 3000;
-// Airtable config
-const opulentBase = new Airtable({apiKey: `${process.env.TOKEN}`}).base(`${process.env.OPULENT_BASE}`);
+
 // Middleware
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
-app.use(express.json());   
-app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Airtable config
+const opulentBase = new Airtable({ apiKey: `${process.env.TOKEN}` }).base(
+	`${process.env.OPULENT_BASE}`
+);
+
 // Tables config
 const attendantsTable = opulentBase('Attendant');
 const pilotsTable = opulentBase('Pilot');
 const contactTable = opulentBase('Contact');
 // Cloudinary config
 cloudinary.config({
-  cloud_name: `${process.env.CLOUDINARY_CLOUD_NAME}`,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+	cloud_name: `${process.env.CLOUDINARY_CLOUD_NAME}`,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 // Main route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+	res.sendFile(path.join(__dirname, '../index.html'));
 });
 // @route     : Attendant table
 // @origin    : /attendant form
 app.post('/attendant', async (req, res) => {
-  upload(req, res, async (err) => {
-    try {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Error uploading file');
-      }
-      const hasValidLicense = req.body.validLicense === 'true';
-      const licenseType = hasValidLicense ? [req.body.licenseSelect] : null
-      const faaResult = req.files['FAAInput'] ? await uploadToCloudinary(req.files['FAAInput'][0]) : null;
-      const cvResult = req.files['cvUpload'] ? await uploadToCloudinary(req.files['cvUpload'][0]) : null;
-      const photoResult = req.files['photoUpload'] ? await uploadToCloudinary(req.files['photoUpload'][0]) : null;
-      const airtableRecord = {
-        fields: {
-          "First_Name": req.body.fname,
-          "Last_Name": req.body.lname,
-          "Email_address": req.body.email,
-          "Phone_number": req.body.phone,
-          "Age": req.body.age,
-          "Gender": req.body.gender,
-          "License_Type": licenseType,
-          "Nationality": req.body.nationality,
-          "Country_of_residence": req.body.residence,
-          "VIP_Experience": req.body.vipExperience,
-          "Valid_licence": req.body.validLicense,
-          "Visas": req.body.visa,
-          "Aircraft_Type": req.body.aircraftSelect,
-          "Country_ICAO": req.body.icaoInput,
-          "FAA_Verification": faaResult ? [
-            {
-              "url": faaResult.url,
-            },
-          ]
-          : null,
-          "Permanent": req.body.permanent,
-          "Freelance": req.body.freelance,
-          "Photo_upload": photoResult ? [
-            {
-              "url": photoResult.url,
-            },
-          ]
-          : null,
-          "CV_File": cvResult ? [
-            {
-              "url": cvResult.url,
-            },
-          ]
-          : null,
-        },
-      };
-      await attendantsTable.create([airtableRecord]);
-      console.log('It worked')
-      res.send('Record created successfully');
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error creating record');
-    }
-  });
+	upload(req, res, async (err) => {
+		try {
+			if (err) {
+				console.error(err);
+				return res.status(500).send('Error uploading file');
+			}
+			console.log(req.body.licenseSelect);
+
+			const hasValidLicense = req.body.validLicense === 'true';
+			const licenseType = hasValidLicense
+				? [req.body.licenseSelect]
+				: null;
+			const faaResult = req.files['FAAInput']
+				? await uploadToCloudinary(req.files['FAAInput'][0])
+				: null;
+			const cvResult = req.files['cvUpload']
+				? await uploadToCloudinary(req.files['cvUpload'][0])
+				: null;
+			const photoResult = req.files['photoUpload']
+				? await uploadToCloudinary(req.files['photoUpload'][0])
+				: null;
+			const airtableRecord = {
+				fields: {
+					First_Name: req.body.fname,
+					Last_Name: req.body.lname,
+					Email_address: req.body.email,
+					Phone_number: req.body.phone,
+					Age: req.body.age,
+					Gender: req.body.gender,
+					License_Type: req.body.licenseSelect,
+					Nationality: req.body.nationality,
+					Country_of_residence: req.body.residence,
+					VIP_Experience: req.body.vipExperience,
+					Valid_licence: req.body.validLicense,
+					Visas: req.body.visa,
+					Aircraft_Type: req.body.aircraftSelect,
+					Country_ICAO: req.body.icaoInput,
+					FAA_Verification: faaResult
+						? [
+								{
+									url: faaResult.url,
+								},
+						  ]
+						: null,
+					Permanent: req.body.permanent,
+					Freelance: req.body.freelance,
+					Photo_upload: photoResult
+						? [
+								{
+									url: photoResult.url,
+								},
+						  ]
+						: null,
+					CV_File: cvResult
+						? [
+								{
+									url: cvResult.url,
+								},
+						  ]
+						: null,
+				},
+			};
+			await attendantsTable.create([airtableRecord]);
+			res.send('Record created successfully');
+		} catch (error) {
+			console.error(error);
+			res.status(500).send('Error creating record');
+		}
+	});
 });
 // @route     : Pilot table
 // @origin    : /pilot form
 app.post('/pilot', async (req, res) => {
-  upload(req, res, async (err) => {
-    try {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Error uploading file');
-      }
-      const hasValidLicense = req.body.validLicense === 'true';
-      const licenseType = hasValidLicense ? [req.body.licenseSelect] : null
-
-      const faaResult = req.files['FAAInput'] ? await uploadToCloudinary(req.files['FAAInput'][0]) : null;
-      const cvResult = req.files['cvUpload'] ? await uploadToCloudinary(req.files['cvUpload'][0]) : null;
-      const photoResult = req.files['photoUpload'] ? await uploadToCloudinary(req.files['photoUpload'][0]) : null;
-      const airtableRecord = {
-        fields: {
-          "First_Name": req.body.fname,
-          "Last_Name": req.body.lname,
-          "Email_address": req.body.email,
-          "Phone_number": req.body.phone,
-          "Age": req.body.age,
-          "Gender": req.body.gender,
-          "License_Type": licenseType,
-          "Nationality": req.body.nationality,
-          "Country_of_residence": req.body.residence,
-          "Total_Time": req.body.totalTime,
-          "Command_Time": req.body.commandTime,
-          "Time_in_Second_Command": req.body.secondTime,
-          "Single_Engine_Land": req.body.singleEngine,
-          "Multi_Engine_Land": req.body.multiEngine,
-          "Jet_Time": req.body.jetTime,
-          "Turbine_Time": req.body.turbineTime,
-          "Total_Helicopter_Hours": req.body.heliHours,
-          "Total_Instructor_Hours": req.body.instructorHours,
-          "Valid_licence": req.body.validLicense,
-          "Aircraft_Type": req.body.aircraftSelect,
-          "Country_ICAO": req.body.icaoInput,
-          "Visas": req.body.visa,
-          "FAA_Verification": faaResult ? [
-            {
-              "url": faaResult.url,
-            },
-          ]
-          : null,
-         
-          "Permanent": req.body.permanent,
-          "Freelance": req.body.freelance,
-         
-          "Photo_upload": photoResult ? [
-            {
-              "url": photoResult.url,
-            },
-          ]
-          : null,
-          "CV_File": cvResult ? [
-            {
-              "url": cvResult.url,
-            },
-          ]
-          : null,
-        },
-      };
-      console.log('License_Type for Pilot:', req.body.licenseSelect);
-      await pilotsTable.create([airtableRecord]);
-      console.log('It worked')
-      res.send('Record created successfully');
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error creating record');
-    }
-  });
+	upload(req, res, async (err) => {
+		try {
+			if (err) {
+				console.error(err);
+				return res.status(500).send('Error uploading file');
+			}
+			const hasValidLicense = req.body.validLicense === 'true';
+			const licenseType = hasValidLicense
+				? [req.body.licenseSelect]
+				: null;
+			const faaResult = req.files['FAAInput']
+				? await uploadToCloudinary(req.files['FAAInput'][0])
+				: null;
+			const cvResult = req.files['cvUpload']
+				? await uploadToCloudinary(req.files['cvUpload'][0])
+				: null;
+			const photoResult = req.files['photoUpload']
+				? await uploadToCloudinary(req.files['photoUpload'][0])
+				: null;
+			const airtableRecord = {
+				fields: {
+					First_Name: req.body.fname,
+					Last_Name: req.body.lname,
+					Email_address: req.body.email,
+					Phone_number: req.body.phone,
+					Age: req.body.age,
+					Gender: req.body.gender,
+					License_Type: req.body.licenseSelect,
+					Nationality: req.body.nationality,
+					Country_of_residence: req.body.residence,
+					Valid_licence: req.body.validLicense,
+					Visas: req.body.visa,
+					Aircraft_Type: req.body.aircraftSelect,
+					Country_ICAO: req.body.icaoInput,
+					FAA_Verification: faaResult
+						? [
+								{
+									url: faaResult.url,
+								},
+						  ]
+						: null,
+					Permanent: req.body.permanent,
+					Freelance: req.body.freelance,
+					Photo_upload: photoResult
+						? [
+								{
+									url: photoResult.url,
+								},
+						  ]
+						: null,
+					CV_File: cvResult
+						? [
+								{
+									url: cvResult.url,
+								},
+						  ]
+						: null,
+				},
+			};
+			await attendantsTable.create([airtableRecord]);
+			res.send('Record created successfully');
+		} catch (error) {
+			console.error(error);
+			res.status(500).send('Error creating record');
+		}
+	});
 });
 // @route     : Contact table
 // @origin    : /contact form
 app.post('/contact', async (req, res) => {
-  upload(req, res, async (err) => {
-    try {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Error uploading file');
-      }
+	upload(req, res, async (err) => {
+		try {
+			if (err) {
+				console.error(err);
+				return res.status(500).send('Error uploading file');
+			}
 
-      const airtableRecord = {
-        fields: {
-          "First Name": req.body.fname,
-          "Last Name": req.body.lname,
-          "Email address": req.body.email,
-          "Phone number": req.body.phone,
-          "Message": req.body.message,
-        },
-      };
+			const airtableRecord = {
+				fields: {
+					'First Name': req.body.fname,
+					'Last Name': req.body.lname,
+					'Email address': req.body.email,
+					'Phone number': req.body.phone,
+					Message: req.body.message,
+				},
+			};
 
-      await contactTable.create([airtableRecord]);
-      console.log('It worked')
-      console.log(req.body)
-      res.send('Record created successfully');
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error creating record');
-    }
-  });
+			await contactTable.create([airtableRecord]);
+			console.log('It worked');
+			console.log(req.body);
+			res.send('Record created successfully');
+		} catch (error) {
+			console.error(error);
+			res.status(500).send('Error creating record');
+		}
+	});
 });
 // Uploads files to cloudinary
 async function uploadToCloudinary(file) {
-  const streamifier = require('streamifier');
-  const bufferStream = streamifier.createReadStream(file.buffer)
- 
-  return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      { resource_type: 'raw' },
-      (error, result) => {
-        if (error) reject(error);
-        else resolve(result);
-      }
-    );
+	const streamifier = require('streamifier');
+	const bufferStream = streamifier.createReadStream(file.buffer);
 
-    bufferStream.pipe(stream);
-  });
+	return new Promise((resolve, reject) => {
+		const stream = cloudinary.uploader.upload_stream(
+			{ resource_type: 'raw' },
+			(error, result) => {
+				if (error) reject(error);
+				else resolve(result);
+			}
+		);
 
+		bufferStream.pipe(stream);
+	});
 }
 app.get('/operatorData', async (req, res) => {
-  try {
-    const attendantRecords = await fetchRecords(attendantsTable);
-    const pilotRecords = await fetchRecords(pilotsTable);
-    // const contactRecords = await fetchRecords(contactTable);
+	try {
+		const attendantRecords = await fetchRecords(attendantsTable);
+		const pilotRecords = await fetchRecords(pilotsTable);
+		// const contactRecords = await fetchRecords(contactTable);
+		console.log(process.env.TOKEN);
 
-    const allRecords = {
-      attendants: attendantRecords,
-      pilots: pilotRecords,
-    };
-    res.sendFile(path.join(__dirname, '../dist/operator.html')); // Sending the file directly
-    res.end(JSON.stringify(allRecords)); // Sending the data separately (optional)
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error fetching records');
-  }
-})
+		const allRecords = {
+			attendants: attendantRecords,
+			pilots: pilotRecords,
+		};
+		res.sendFile(path.join(__dirname, '../dist/operator.html')); // Sending the file directly
+		res.end(JSON.stringify(allRecords)); // Sending the data separately (optional)
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Error fetching records');
+	}
+});
 async function fetchRecords(table) {
-  return new Promise((resolve, reject) => {
-    table.select().firstPage((err, records) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(records);
-      }
-    });
-  });
+	return new Promise((resolve, reject) => {
+		table.select().firstPage((err, records) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(records);
+			}
+		});
+	});
 }
 app.listen(process.env.PORT);
-
-
